cb0bf8c215450c617c04ac06dea2606bc56cda79
Jesus Pastor <jnavalon@biit-solutions.com>
2024-07-13 11:43:11 +0200

    fix: workarround fixing issue with transloco (possibly?)

--- src/app/views/biit-login-page/biit-login-page.module.ts
+++ src/app/views/biit-login-page/biit-login-page.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BiitLoginPageComponent } from './biit-login-page.component';
 import {BiitLoginModule} from "biit-ui/login";
 import {BiitProgressBarModule} from "biit-ui/info";
 import {TranslocoRootModule} from "biit-ui/i18n";
 import {BiitLoginPageRoutingModule} from "./biit-login-page-routing.module";
+import {TRANSLOCO_CONFIG, TRANSLOCO_LOADER, translocoConfig} from "@ngneat/transloco";
+import {TranslocoHttpLoader} from "../../transloco-root.module";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HeaderInterceptor} from "../../config/header-interceptor";
 
 
 
@@ -21,6 +25,20 @@ import {BiitLoginPageRoutingModule} from "./biit-login-page-routing.module";
     BiitLoginModule,
     TranslocoRootModule,
     BiitProgressBarModule
+  ],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs: ['en', 'es', 'nl'],
+        defaultLang: 'en',
+        fallbackLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode()
+      })
+    },
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoader },
+    {provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true}
   ]
 })
 export class BiitLoginPageModule { }
