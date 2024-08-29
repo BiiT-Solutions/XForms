57f9d79b308c1bac2530a044c5ffdf7ff798a3d2
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-08-29 11:29:18 +0200

    Updating docker URLs

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,8 +1,8 @@
 export class Environment {
   public static readonly ROOT_URL: string = 'DOCKER:PROTOCOL://DOCKER:MACHINE_DOMAIN';
   public static readonly FORM_PATH: string = '/events/topics/form';
-  public static readonly KAFKA_PROXY_URL: string = 'DOCKER:PROTOCOL://DOCKER:KAFKA_PROXY_URL';
+  public static readonly KAFKA_PROXY_URL: string = 'DOCKER:KAFKA_PROXY_URL';
   public static readonly USER_MANAGER_URL: string = 'DOCKER:USER_MANAGER_URL';
-  public static readonly WEB_FORMS_URL: string = 'DOCKER:PROTOCOL://DOCKER:WEBFORMS_URL';
+  public static readonly WEB_FORMS_URL: string = 'DOCKER:WEBFORMS_URL';
   public static readonly WEB_FORMS_CREDENTIALS: string = 'DOCKER:WEBFORMS_USER:DOCKER:WEBFORMS_PASSWORD';
 }
