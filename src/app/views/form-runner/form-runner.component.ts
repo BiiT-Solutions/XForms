0728a319b75473d290d8d9736a33225fc4c7f0d9
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-03-18 10:54:04 +0100

    Added extra logs

--- src/app/views/form-runner/form-runner.component.ts
+++ src/app/views/form-runner/form-runner.component.ts
@@ -77,7 +77,7 @@ export class FormRunnerComponent implements OnInit, AfterViewInit {
                   next: (form: Form): void => {
                     this.unprocessedForm = form;
                     this.form = Form.import(form, this.getMapFromParams(params));
-                    console.info('Form loaded from remote webforms server.');
+                    console.debug('Form loaded from remote webforms server.');
                   },
                   error: (): void => {
                     console.error('Form was not found on remote service. We are trying to check the deployed ones.');
@@ -106,7 +106,7 @@ export class FormRunnerComponent implements OnInit, AfterViewInit {
         next: (form: any): void => {
           this.unprocessedForm = form;
           this.form = Form.import(form, this.getMapFromParams(params));
-          console.info('Form loaded from local assets.');
+          console.debug('Form loaded from local assets.');
         },
         error: (): boolean => this.loading = false
       });
